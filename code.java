8604e687828774f84719dd4e6e57a94b45e45caf
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-11-01 15:46:30 +0100

    Update code.java

---
+++ code.java
@@ -0,0 +1,25 @@
+public class CyrilClassRooms {
+    //int pour un nombre entier
+    
+        public static void main(String[] args) {
+            int agePersonne = 19;
+            agePersonne = 33;
+    
+            agePersonne = agePersonne + 28;
+            int incomes = 500;
+    
+            int savings = 1000;
+            
+            //chaque instruction java se termine par ;
+            
+            float help = 0.5;
+            
+            // float pour des nomre decimaux ta aussi le"double"
+            
+            System.out.printin("Age = " + agePersonne + * ans*);
+        }
+    
+    }
+    
+    
+    
