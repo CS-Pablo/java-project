b8d82aa5a287db8a30ad673564455b8eea5691ef
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-11-01 15:47:21 +0100

    Update reg.java

---
+++ reg.java
@@ -0,0 +1,17 @@
+public class ManipulationVariablesNUM {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        float b = 2;
+
+        float c = a / b;
+
+        System.out.printIn("Resultat :" + c);
+
+
+
+    }
+
+}
