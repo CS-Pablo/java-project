0344e9e0037bd8e1a611db6b91ea63a622d46382
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-11-01 15:46:54 +0100

    Update epargne.java

---
+++ epargne.java
@@ -0,0 +1,41 @@
+ 
+
+    int epargne = 500;
+    int revenues = 2000;
+    //Ajoutez 100 à votre épargne (Yeah!)
+    
+    epargne = epargne + 100;
+
+    //enleever 50 a votre indemnité
+    
+    revenues = revenues - 50;
+
+    //Faites une mise a jour sur vos delais d'épargne
+
+    int nombreDeJourEpargne = (5000 - revenues) / 500;
+
+    //Mettez à jour à nouveau votre indemnité 
+
+    revenues = revenues + (30 - 10) * 7;
+
+    String text = "Hello World By pabloooo";
+
+    final int NUMBEROFWEEKDAYS = 7;
+
+    final String MYFAVOURITEFOOD = "bolognaise";
+
+    int nuberOfPets = 1;
+
+    String currentSeason = "Winter";
+
+    int nombreDeMotsDansLaPhrase;
+
+
+    }
+
+}
+
+//STING = TXT
+
+// LE NOM D'UNE CON?STANTE EN JAVA SERA TJRS EN MAJ
+
