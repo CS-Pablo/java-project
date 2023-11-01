ad8faa3304b4cee11f009ea7a64ad799ca53f043
CS-Pablo <148013823+CS-Pablo@users.noreply.github.com>
2023-11-01 15:47:46 +0100

    Update chap2.java

---
+++ chap2.java
@@ -0,0 +1,13 @@
+public class ManipulationVariables1 {
+
+    public static void main(String[] args) {
+
+double = 1876.79797657;
+
+float = 1876.79;
+
+// FLOAT PAS PLUS DE DEUX APRES LA VIRGULE SINON DOUBLE
+
+    }
+
+}
